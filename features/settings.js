bad1a9ac4b400f721fa34fa5b86268b162634751
andreasthuis <andreasdeborger27@gmail.com>
2025-11-12 11:06:00 +0100

    pushed settings update

--- features/settings.js
+++ features/settings.js
@@ -1,18 +1,21 @@
 (async () => {
-  try {
-    const fileContents = await global.smartschool_webRequest(
-      "GET",
-      "https://raw.githubusercontent.com/andreasthuis/smartschool_edit/main/html/settingsElement.html"
-    );
-    console.log(fileContents);
-  } catch (err) {
-    console.error(err);
-  }
+  const $ = window.jQuery;
+
+  const iframe = $('<iframe>', {
+    src: 'https://raw.githubusercontent.com/andreasthuis/Smartschool_edit/refs/heads/main/html/settingsElement.html',
+    css: {
+      width: '100%',
+      height: '700px',
+      border: 'none',
+      display: 'block'
+    },
+    id: 'smartschool-settings'
+  });
 
   smartschool_addElement({
     parentSelector: '#leftcontainer',
     elementId: 'smartschool-settings',
-    content: fileContents,
+    content: iframe.prop('outerHTML'),
     insertType: 'append'
   });
 })();
